d56d2f5b637406840137561f32679257bc9ad466
Nezo96 <39273050+Nezo96@users.noreply.github.com>
2023-08-19 19:37:23 +0200

    Added about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+function page() {
+  return (
+    <div className="max-w-6xl mx-auto space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet
+        nesciunt odio hic culpa aliquid aliquam nobis cupiditate, quod atque
+        delectus odit laboriosam rerum necessitatibus, repudiandae doloribus?
+        Expedita et cupiditate error!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores
+        pariatur earum nostrum velit maxime cupiditate, harum, atque amet
+        corporis nobis iste ea odit non illum, consequatur veniam voluptatem
+        perspiciatis voluptates.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate sed
+        maxime ab fugiat labore, quasi quaerat fuga totam et vero. Dignissimos,
+        exercitationem laboriosam? Sit architecto accusantium commodi iure,
+        repudiandae nam.
+      </p>
+    </div>
+  );
+}
+
+export default page;
