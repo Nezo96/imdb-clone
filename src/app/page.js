259f09a3889305b85f9127875d32fd77fbc7d4b1
Nezo96 <39273050+Nezo96@users.noreply.github.com>
2023-08-20 15:22:46 +0200

    Added fetching from themoviedb

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,28 @@
-import Image from "next/image";
+import Results from "@/components/Results";
+import React from "react";
 
-export default function Home() {
-  return <h1>Home</h1>;
-}
+const Home = async ({ searchParams }) => {
+  const API_KEY = process.env.API_KEY;
+  const genre = searchParams.genre || "fetchTrending";
+  const res = await fetch(
+    `https://api.themoviedb.org/3/${
+      genre === "fetchTopRated" ? "movie/top_rated" : "trending/all/week"
+    }?api_key=${API_KEY}&language=sk-SK&page=1`,
+    { next: { revalidate: 10 } }
+  );
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  const data = await res.json();
+  const result = data.results;
+  console.log(result);
+  return (
+    <div>
+      <Results results={result} />
+    </div>
+  );
+};
+
+export default Home;
