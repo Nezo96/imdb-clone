7db30a7ec67626f6ac6966ebb7137078dec6c3a4
Nezo96 <39273050+Nezo96@users.noreply.github.com>
2023-08-20 15:40:02 +0200

    Added loading effect

---
+++ src/app/loading.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const loading = () => {
+  return (
+    <div className="flex items-center justify-center">
+      <img className="h-96" src="Rolling.svg" alt="loading..." />
+    </div>
+  );
+};
+
+export default loading;
